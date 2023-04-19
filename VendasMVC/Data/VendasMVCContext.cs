da82f82b75c83c5868a50cea8776952db4870b80
Ranieeery <raniery2003@hotmail.com>
2023-04-19 20:59:01 -0300

    feat - relações e outra migration

--- VendasMVC/Data/VendasMVCContext.cs
+++ VendasMVC/Data/VendasMVCContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace VendasMVC.Models
@@ -13,6 +13,8 @@ namespace VendasMVC.Models
         {
         }
 
-        public DbSet<VendasMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
