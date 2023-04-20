0eab0983a9aa17f8cbc1b82685b91fd33c34e296
Ranieeery <raniery2003@hotmail.com>
2023-04-20 18:09:55 -0300

    feat - seller service e findAll

--- VendasMVC/Startup.cs
+++ VendasMVC/Startup.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using VendasMVC.Models;
 using VendasMVC.Data;
+using VendasMVC.Services;
 
 namespace VendasMVC
 {
@@ -42,6 +43,7 @@ namespace VendasMVC
                         builder.MigrationsAssembly("VendasMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
