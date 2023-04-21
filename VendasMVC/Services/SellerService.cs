bc51c02ac7318ae65d91ebfe4e6761cbde54e003
Ranieeery <raniery2003@hotmail.com>
2023-04-21 17:20:59 -0300

    feat - database fk

--- VendasMVC/Services/SellerService.cs
+++ VendasMVC/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace VendasMVC.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
