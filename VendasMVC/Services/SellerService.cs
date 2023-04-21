4f9586d166e09fb715b500b13cedd1bcf3b0fa0c
Ranieeery <raniery2003@hotmail.com>
2023-04-21 20:39:08 -0300

    feat - details seller

--- VendasMVC/Services/SellerService.cs
+++ VendasMVC/Services/SellerService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using VendasMVC.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace VendasMVC.Services
 {
@@ -28,7 +29,7 @@ namespace VendasMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
