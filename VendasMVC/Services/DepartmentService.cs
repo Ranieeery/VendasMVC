ce13056c7f8043b22a97bdfd74bce29b800f8bc6
Ranieeery <raniery2003@hotmail.com>
2023-04-22 10:37:11 -0300

    feat - async operations

--- VendasMVC/Services/DepartmentService.cs
+++ VendasMVC/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using VendasMVC.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace VendasMVC.Services
 {
@@ -13,9 +15,9 @@ namespace VendasMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
