9136b8747ed5f12cfb198c73cb827424d36765b9
Ranieeery <raniery2003@hotmail.com>
2023-04-21 21:58:46 -0300

    feat - update form and exceptions

---
+++ VendasMVC/Services/Exception/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VendasMVC.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
--- VendasMVC/Services/SellerService.cs
+++ VendasMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using VendasMVC.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using VendasMVC.Services.Exception;
 
 namespace VendasMVC.Services
 {
@@ -37,6 +38,23 @@ namespace VendasMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
