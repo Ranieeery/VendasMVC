e27da766f104475afb47db380814261afd03e625
Ranieeery <raniery2003@hotmail.com>
2023-04-22 11:35:27 -0300

    feat - delete exception

---
+++ VendasMVC/Services/Exception/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasMVC.Services.Exception
+{
+    public class IntegrityException
+    {
+    }
+}
