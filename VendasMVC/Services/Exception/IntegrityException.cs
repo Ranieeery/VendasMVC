b23707578876896d64438736409952a822aa2744
Ranieeery <raniery2003@hotmail.com>
2023-04-22 11:38:57 -0300

    fix - delete exception

--- VendasMVC/Services/Exception/IntegrityException.cs
+++ VendasMVC/Services/Exception/IntegrityException.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace VendasMVC.Services.Exception
 {
-    public class IntegrityException
+    public class IntegrityException : ApplicationException
     {
+        public IntegrityException(string message) : base(message)
+        {
+        }
     }
 }
